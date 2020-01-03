7b9d027d42cf5e3f15625b00999b63aa3fc558a0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 17:44:25 -0500

    Fix mkdir in fabfile

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,10 +13,10 @@ def do_pack():
     now = datetime.datetime.now()
     str_time = now.strftime("%Y%m%d%H%M%S")
     file_name = "web_static_{}".format(str_time)
-    local("mkdir versions")
+    local("mkdir -p versions")
     status_f = local("tar -cvzf versions/{}.tgz web_static".format(file_name))
 
-    if status_f == 0:
+    if status_f.succeeded:
         return "versions/{}.tzg".format(file_name)
     else:
         return None
