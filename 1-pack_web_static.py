b33bb2c3c5f7f0eff27fd5fc6ab44a91fa1c41a0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 17:38:21 -0500

    Removing hosts

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,9 +6,6 @@ from fabric.api import *
 import datetime
 
 
-env.user = 'ubuntu'
-env.hosts = ['35.237.240.111', '104.196.143.231']
-
 def do_pack():
     """
     Compress the content of web_static folder in a .tgz
@@ -16,10 +13,10 @@ def do_pack():
     now = datetime.datetime.now()
     str_time = now.strftime("%Y%m%d%H%M%S")
     file_name = "web_static_{}".format(str_time)
-    run("mkdir versions")
-    status_file = run("tar -cvzf versions/{}.tgz web_static".format(file_name))
+    local("mkdir versions")
+    status_f = local("tar -cvzf versions/{}.tgz web_static".format(file_name))
 
-    if status_file == 0:
+    if status_f == 0:
         return "versions/{}.tzg".format(file_name)
     else:
         return None
