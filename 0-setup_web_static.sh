8d19fe1fb6a438ada3e5c320c3a5ef0782824490
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 16:05:51 -0500

    Refactoring script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ echo "<!DOCTYPE html>
                 <h1>Holberton School</h1>
         </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _;/a location /hbnb_static/ {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 service nginx restart
