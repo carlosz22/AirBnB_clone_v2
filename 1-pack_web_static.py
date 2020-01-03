d70709137cb8bac11b8c31ab0d666e4f640794bd
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 17:33:01 -0500

    Add hosts to fabfile

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,16 @@ from fabric.api import *
 import datetime
 
 
+env.user = 'ubuntu'
+env.hosts = ['35.237.240.111', '104.196.143.231']
+
 def do_pack():
     """
     Compress the content of web_static folder in a .tgz
     """
     now = datetime.datetime.now()
-    str_time = now.strtime("%Y%m%d%H%M%S")
-    file_name = "web_static{}".format(str_time)
+    str_time = now.strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}".format(str_time)
     run("mkdir versions")
     status_file = run("tar -cvzf versions/{}.tgz web_static".format(file_name))
 
