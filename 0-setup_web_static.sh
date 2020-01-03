808cec165294d6ba2ae86e4921fe8be5017929b0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 15:19:11 -0500

    Fix script to include the creation of all directories

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,20 +3,21 @@
 
 apt-get -y update
 apt-get install -y nginx 
-mkdir -p /data/web-static/releases/test
-mkdir -p /data/web-static/shared
-mkdir -p /data/web-static/current
+mkdir -p /data/
+mkdir -p /data/web-static/
+mkdir -p /data/web-static/releases/test/
+mkdir -p /data/web-static/shared/
+mkdir -p /data/web-static/current/
 echo "<!DOCTYPE html>
 <html>
         <head>
-                <title>Hi!</title>
+                <title>Holberton School</title>
         </head>
         <body>
-                <h1>Hi!</h1>
+                <h1>Holberton School</h1>
         </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web-static/releases/test /data/web-static/current
-chown -R ubuntu /data
-chgrp -R ubuntu /data
+chown -R ubuntu:ubuntu /data
 sed -i "/server_name _;/a location /hbnb_static {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 service nginx restart
