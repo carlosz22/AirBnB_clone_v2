7e5ab684a02e090656c10568e9dac69dc609611e
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 16:05:14 -0500

    Refactoring script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,5 +19,5 @@ echo "<!DOCTYPE html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "/server_name _;/a location /hbnb_static {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
+sed -i "/server_name _;/a location /hbnb_static/ {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 service nginx restart
