a753295e87e3274f9a56312691fc5753570ef92b
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-03 15:44:13 -0500

    Refactoring script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,13 @@
 # Install Nginx and configure it
 
 apt-get -y update
-apt-get install -y nginx 
+apt-get install -y nginx
 mkdir -p /data/
-mkdir -p /data/web-static/
-mkdir -p /data/web-static/releases/test/
-mkdir -p /data/web-static/shared/
-mkdir -p /data/web-static/current/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/current/
 echo "<!DOCTYPE html>
 <html>
         <head>
@@ -17,7 +18,8 @@ echo "<!DOCTYPE html>
                 <h1>Holberton School</h1>
         </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web-static/releases/test /data/web-static/current
-chown -R ubuntu:ubuntu /data
+ln -sf /data/web_static/releases/test/ /data/web_static/current/
+chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _;/a location /hbnb_static {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 service nginx restart
+
